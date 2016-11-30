34c06188b9dfe5fc2202cbb7866dcc11aa65b436
LilianaCanPor <liliana.canpor@gmail.com>
2016-11-29 23:49:47 -0600

    entregable

--- js/scripts.js
+++ js/scripts.js
@@ -9,5 +9,20 @@ $(document).ready(function(){
 		}
 		isShowing = !isShowing;	
 	});
-	
+
+	$('#mail').hide();
+	$('#phone').hide();	
+	$('.icon-envelop').mouseenter(function(){
+		$('#mail').show(200);
+	});
+	$('.icon-envelop').mouseout(function(){
+		$('#mail').hide();
+	});
+	$('.icon-phone').mouseenter(function(){
+		$('#phone').show(200);
+	});
+	$('.icon-phone').mouseout(function(){
+		$('#phone').hide();
+	});
+
 });
